10cc9d8873cf6551eaf8bb6d923ff264c939df89
arpitparekh <arpitparekh9@gmail.com>
2025-06-14 09:35:32 +0530

    first commit

---
+++ Introduction.java
@@ -0,0 +1,52 @@
+public class Introduction {
+  public static void main(String[] args) {
+
+    System.out.println("Hello World");
+
+    // primitive datatype
+    // 8 primitive datatypes
+    byte b = 23;
+    short sh = 45;
+    int i = 454545;
+    long l = 454545454545L;
+
+    float f = 12.34f;
+    double d = 34343434.343434;
+
+    char c = '?';
+    boolean bl = true;
+
+    // conditional statements
+
+    // if else if else
+    // switch
+
+    int abc = 45;
+
+    if (abc < 23) {
+      System.out.println("abc is less than 23");
+    } else if (abc > 23) {
+    } else {
+
+    }
+
+
+    int num = 55;
+
+    switch (num) {
+      case 1:
+        System.out.println("num is 1");
+        break;
+      case 2:
+        System.out.println("num is 2");
+        break;
+      case 3:
+        System.out.println("num is 3");
+        break;
+      default:
+        System.out.println("num is not 1,2,3");
+        break;
+    }
+
+  }
+}
